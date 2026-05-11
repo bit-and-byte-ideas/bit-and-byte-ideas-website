91f9ae7d9349c4e226f7ef0fa6d30ed66b8f88a5
Carlos Barajas <soldotsol@gmail.com>
2026-05-10 20:27:43 -0700

    chore: add env value to module

--- deploy/infra/dev/main.tf
+++ deploy/infra/dev/main.tf
@@ -1,9 +1,9 @@
 module "static_web_app" {
   source = "github.com/bit-and-byte-ideas/azure-static-webapp-cicd-kit/modules/azure-static-webapp"
 
-  resource_group_name = "rg-bit-and-byte-ideas"
+  resource_group_name = "rg-bit-and-byte-ideas-dev"
   location            = "West US 2"
-  static_webapp_name  = "bit-and-byte-ideas"
+  static_webapp_name  = "bit-and-byte-ideas-dev"
 
   # Optional features
   sku_tier                    = "Free"
