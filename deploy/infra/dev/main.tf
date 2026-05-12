f0bd2ff23543903ab71d1cc5171af9d3b9d84c98
Carlos Barajas <soldotsol@gmail.com>
2026-05-11 20:42:55 -0700

    fix: environment var ref

--- deploy/infra/dev/main.tf
+++ deploy/infra/dev/main.tf
@@ -1,7 +1,7 @@
 module "static_web_app" {
   source                      = "github.com/bit-and-byte-ideas/azure-static-webapp-cicd-kit/modules/azure-static-webapp"
-  resource_group_name         = "${var.project_name}-${var.tags[environment]}-rg"
-  static_webapp_name          = "${var.project_name}-${var.tags[environment]}-swa"
+  resource_group_name         = "${var.project_name}-${var.environment}-rg"
+  static_webapp_name          = "${var.project_name}-${var.environment}-swa"
   location                    = var.location
   sku_tier                    = var.sku_tier
   enable_application_insights = var.application_insights
